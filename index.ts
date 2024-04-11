9b021fdfbaa805a245cfe4c1d94cab417caa78a0
rezaaliabidi <syedrezaaliabidi@gmail.com>
2024-04-11 14:03:05 -0700

    word count

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answers: {
+
+    Sentence: string
+
+} = await inquirer .prompt([
+
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your senetence to count the word:",
+    }
+])
+
+const words = answers.Sentence.trim( ).split(" ")
+console.log(`Your sentence word count is ${words.length}`)
